80b9d03dbefb431309f810189b0a01c2d8f1ce84
Nuambevos <ruslan.bilokoniuk@gmail.com>
2023-11-26 13:58:36 +0200

    package creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clean_folder',
+      version='1',
+      description='This code automatically organizes files in a specified directory based on their types and categories. It uses predefined dictionaries for classifying files into groups such as images, videos, documents, audio, archives, and others. Additionally, it includes functions for normalizing file names, handling duplicates, moving files to appropriate folders, and searching for files in the specified directory.',
+      url='https://github.com/Nyambevos/Sorter_for_files',
+      author='Nuambevos',
+      author_email='ruslan.bilokoniuk@gmail.com',
+      license='MIT',
+      entry_points={'console_scripts': ['clean_folder = clean_folder.clean:main']})
